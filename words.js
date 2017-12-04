b4ae81551a09b25178ee7bfc80039b47cde86383
Brandon York <30988963+byork2005@users.noreply.github.com>
2017-12-04 16:54:46 -0600

    update 12/4

---
+++ words.js
@@ -0,0 +1,9 @@
+var brokenWord = require("./brokenWord.js");
+
+var newWord = function(word) {
+    var letters = this.word.split("");
+    for (var i = 0; i < letters.length; i++)
+    {
+        var currentWord = new brokenWord(letter[i]);
+    }
+}
